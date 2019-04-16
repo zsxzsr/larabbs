2d0c773c36a1d5fe88b6734a12eadbf6f3aa48d2
zsx <47313271@qq.com>
2019-04-16 09:49:39 +0000

    基础页面结构

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">
+    <div class="container">
+        {{--Branding Image--}}
+        <a class="navbar-brand" href="{{url('/')}}">
+            laraBBS
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            {{--left side of navbar--}}
+            <ul class="navbar-nav mr-auto">
+
+            </ul>
+
+            {{--right side of navbar--}}
+            <ul class="navbar-nav navbar-right">
+                {{--Authentication Links--}}
+                <li class="nav-item"><a class="nav-link" href="#">登陆</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">注册</a></li>
+            </ul>
+        </div>
+
+    </div>
+
+</nav>
