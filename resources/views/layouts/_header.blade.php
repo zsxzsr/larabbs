208e1eda6c2b3bed715874165229c271dd858bf7
zsx <47313271@qq.com>
2019-04-17 02:28:11 +0000

    生成用户认证代码

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">
     <div class="container">
-        {{--Branding Image--}}
+        <!--Branding Image-->
         <a class="navbar-brand" href="{{url('/')}}">
             laraBBS
         </a>
@@ -9,16 +9,16 @@
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            {{--left side of navbar--}}
+            <!--left side of navbar-->
             <ul class="navbar-nav mr-auto">
 
             </ul>
 
-            {{--right side of navbar--}}
+            <!--right side of navbar-->
             <ul class="navbar-nav navbar-right">
-                {{--Authentication Links--}}
-                <li class="nav-item"><a class="nav-link" href="#">登陆</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">注册</a></li>
+                <!--Authentication Links-->
+                <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登陆</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{route('register')}}">注册</a></li>
             </ul>
         </div>
 
