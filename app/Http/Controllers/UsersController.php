519e3ea8f8423de7b9eef0097099564b9e54d0fd
诺克 <47313271@qq.com>
2019-04-22 11:38:58 +0800

    显示个人资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,12 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Http\Requests\UserRequest;
 
 class UsersController extends Controller
 {
-    //
+    //个人详情页
     public function show(User $user)
     {
         return view('users.show',compact('user'));
+    }
+
+    //编辑页面
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    //更新用户信息
+    public function update(UserRequest $request, User $user)
+    {
+
+        $user->update($request->all());
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 }
