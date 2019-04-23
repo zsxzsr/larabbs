305e9eaa5ed40b4afb4274e79729c3894f277ece
zsx <47313271@qq.com>
2019-04-23 08:33:37 +0000

    分类话题列表

--- routes/web.php
+++ routes/web.php
@@ -41,3 +41,6 @@ Route::get('email/resend', 'Auth\VerificationController@resend')->name('verifica
 //用户相关路由
 Route::resource('users','UsersController',['only'=>['show','update','edit']]);
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+//帖子分类路由
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
