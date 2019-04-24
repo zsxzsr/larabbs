f80eb856b52c1704bfd7534eb325d2e6cd483430
zsx <47313271@qq.com>
2019-04-24 08:52:41 +0000

    回复数据

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,9 @@ class User extends Authenticatable implements MustVerifyEmailContract
     {
         return $this->hasMany(Topic::class);
     }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -47,3 +47,4 @@ Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
 //帖子图片路由
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
+Route::resource('replies', 'RepliesController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
