58ab6c872968363b1d818f54b5e16725f96a3fc8
zsx <47313271@qq.com>
2019-04-25 06:41:01 +0000

    fixbug

--- resources/views/notifications/types/_topic_replied.blade.php
+++ resources/views/notifications/types/_topic_replied.blade.php
@@ -9,7 +9,7 @@
         <div class="media-heading mt-0 mb-1 text-secondary">
             <a href="{{ route('users.show', $notification->data['user_id']) }}">{{ $notification->data['user_name'] }}</a>
             评论了
-            <a href="{{ $notification->data['topic_link'] }}">{{ $notification->data['topic_title'] }}</a>
+            <a href="{{ route('topics.show',$notification->data['topic_id']) }}">{{ $notification->data['topic_title'] }}</a>
 
             {{-- 回复删除按钮 --}}
             <span class="meta float-right" title="{{ $notification->created_at }}">
