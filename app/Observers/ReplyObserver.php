eab196ece6f96390c128d2b7561f584fa63e0918
zsx <47313271@qq.com>
2019-04-25 06:11:02 +0000

    消息通知有新回复

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -3,7 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Reply;
-
+use App\Notifications\TopicReplied;
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
@@ -13,8 +13,15 @@ class ReplyObserver
     {
         $reply->content = clean($reply->content, 'user_topic_body');
 //        $reply->topic->increment('reply_count', 1);
+
+    }
+
+    public function created(Reply $reply)
+    {
         $reply->topic->reply_count = $reply->topic->replies->count();
         $reply->topic->save();
+        // 通知话题作者有新的评论
+        $reply->topic->user->notify(new TopicReplied($reply));
     }
 
     public function updating(Reply $reply)
