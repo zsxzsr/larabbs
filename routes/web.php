884a7f23f840854db23ab830f694b252934cef88
zsx <47313271@qq.com>
2019-04-25 03:26:30 +0000

    用户可以发表评论

--- routes/web.php
+++ routes/web.php
@@ -47,4 +47,6 @@ Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
 //帖子图片路由
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
-Route::resource('replies', 'RepliesController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+//帖子回复
+Route::resource('replies', 'RepliesController', ['only' => ['store','destroy']]);
