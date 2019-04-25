4ed179f0d731cb10a82f45101f2ffbec81b0c7b5
zsx <47313271@qq.com>
2019-04-25 09:28:55 +0000

    集成 laravel-prtmission 扩展包

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,8 +7,11 @@ use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 use Auth;
+use Spatie\Permission\Traits\HasRoles;
+
 class User extends Authenticatable implements MustVerifyEmailContract
 {
+    use HasRoles;
     use MustVerifyEmailTrait;
     use Notifiable {
         notify as protected laravelNotify;
